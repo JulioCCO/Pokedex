d087b06e7947e29fa8c09abe222bd18640c380e4
Jose Serrano Quesada <joseserranoq00@gmail.com>
2023-04-10 23:42:51 -0600

    Dis-32
    80% completed

--- api/fetchRandomPokemon.js
+++ api/fetchRandomPokemon.js
@@ -1,36 +1,27 @@
-import fetchMoves from "./fetchMoves";
 
-export const fetchEnemy = async () => {
-    const randomPkm = Math.floor(Math.random() * 649);
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPkm}`)
+export const fetchRandPokemons = async () => {
+    pokeInfoList = [];
+    let i = 0;
+    while( i<4){   
+        const randomPkm = Math.floor(Math.random() * 649);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPkm}`) 
+        if (!response.ok) {
+            throw new Error('Failed to fetch Moves');
+        }
+        const results = await response.json();  
+        const pokeInfo = {
+            name: results.name,
+            official: results.sprites.other["official-artwork"].front_default,
+        };
+        //console.log(pokemon.name);
+        if(!pokeInfoList.includes(pokeInfo)){
+            console.log(pokeInfo.name + ' no está en la lista')
+            //setPokeList({lista: [...pokeList.lista, pokemon], random: ""});
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch Moves');
-
+            pokeInfoList.push(pokeInfo);
+            ++i; 
+          }
     }
-    const results = await response.json();
-    const moveslist = []
-    for (let index = 0; index < 4; index++) {
-        let randomMoveNumber = Math.floor(Math.random() * results.moves.length);
-        const resp = await fetchMoves(results.moves[randomMoveNumber].move.url);
-        moveslist.push({
-            name: resp.name,
-            accuracy: resp.accuracy,
-            power: resp.power,
-            pp: resp.pp,      
-        });
-
-    }
-
-    
-    const pokeInfo = {
-        name: results.name,
-        abilities: results.abilities,
-        moves: moveslist,
-        types: results.types,
-        sprites: results.sprites,
-    };
-    return pokeInfo;
-    
-    
+    //pokeRand = pokeInfoList[Math.floor(Math.random() * 4)];
+    return pokeInfoList;
 }
